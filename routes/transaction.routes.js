9afb78672fc55a2ddff49695556a66e6bef9c4ed
Hi-mahammed <Hi-mahammed@proton.me>
2026-06-08 00:51:45 +0300

    Update transaction.routes.js

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -3,6 +3,7 @@ const {
   addTransaction,
   getPendingTransactions,
   getAllTransactions,
+  signAndAddTransaction,
 } = require('../controllers/transaction.controller');
 const { validateBody } = require('../middleware/validateRequest.middleware');
 const { writeLimiter } = require('../middleware/rateLimit.middleware');
@@ -10,6 +11,7 @@ const { writeLimiter } = require('../middleware/rateLimit.middleware');
 const router = Router();
 
 router.post('/', writeLimiter, validateBody(['fromAddress', 'toAddress', 'amount']), addTransaction);
+router.post('/sign', writeLimiter, validateBody(['fromAddress', 'toAddress', 'amount', 'privateKeyHex']), signAndAddTransaction);
 router.get('/pending', getPendingTransactions);
 router.get('/all', getAllTransactions);
 
